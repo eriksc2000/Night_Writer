3cdee61ab4cefa3bb86d49e35f408b0840d27d1e
Erik Schneider <erikschneider2211@gmail.com>
2018-07-08 14:20:18 -0600

    adds multiple characters test

--- test/braille_map_test.rb
+++ test/braille_map_test.rb
@@ -16,4 +16,12 @@ class BrailleMapTest < Minitest::Test
     assert_equal "0.....", braille.translate("a")
   end
   
+  def test_it_converts_multiple_characters_into_braille
+    braille = BrailleMap.new 
+    expected = "0.....0.0...00...."
+    actual = braille.translate('abc')
+    
+    assert_equal expected, actual
+  end
+  
 end
